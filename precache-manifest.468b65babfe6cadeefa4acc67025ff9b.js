ffe98783eecc6a55ad3d273343a8071697a53602
sum1818 <sum1818@163.com>
2020-10-30 15:12:20 +0800

    1111111111

---
+++ precache-manifest.468b65babfe6cadeefa4acc67025ff9b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b78c4f6be9b1656e9b6bf3ca42fe35",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/index.html"
+  },
+  {
+    "revision": "90dbe24a49644cd1486f",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/static/css/2.63d38472.chunk.css"
+  },
+  {
+    "revision": "eaf1de60922c0e52d0ec",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/static/css/main.90d59b5a.chunk.css"
+  },
+  {
+    "revision": "90dbe24a49644cd1486f",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/static/js/2.246defe8.chunk.js"
+  },
+  {
+    "revision": "328b6868efc601464a5a18673397ef38",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/static/js/2.246defe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf1de60922c0e52d0ec",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/static/js/main.6b2e633a.chunk.js"
+  },
+  {
+    "revision": "75a7f7d85e71c155ff67",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/static/js/runtime-main.3b258e86.js"
+  },
+  {
+    "revision": "16268e0674d5cd6b9a2f2232c7f7d5d7",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/static/media/beijing.16268e06.png"
+  },
+  {
+    "revision": "b1d57687db57d805fbbdf9b1f818ab21",
+    "url": "https://cdn.jsdelivr.net/gh/zhanghao1818/hlztest@1.0.0/static/media/download_shale.b1d57687.png"
+  }
+]);
